e51ea22e233d8134fd3d79d9e015dce78adc4a55
aynur.gimadutdinov <aynur.gimadutdinov@shtormtech.ru>
2022-01-05 10:53:54 +0300

    hw10-byteCodes

--- hw10-byteCodes/src/main/java/ru/asamara56/Demo.java
+++ hw10-byteCodes/src/main/java/ru/asamara56/Demo.java
@@ -3,7 +3,7 @@ package ru.asamara56;
 public class Demo {
 
     public static void main(String[] args) {
-        var object = Ioc.createClass();
+        var object = Ioc.createTestLogging();
 
         object.calculation(10);
         object.calculation(10, 20);
--- hw10-byteCodes/src/main/java/ru/asamara56/Ioc.java
+++ hw10-byteCodes/src/main/java/ru/asamara56/Ioc.java
@@ -5,15 +5,15 @@ import ru.asamara56.annotation.Log;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Ioc {
 
     private Ioc() {}
 
-    static TestLogging createClass() {
+    static TestLogging createTestLogging() {
         InvocationHandler handler = new DemoInvocationHandler(new TestLoggingImpl());
         return (TestLogging) Proxy.newProxyInstance(Demo.class.getClassLoader(),
                 new Class<?>[]{TestLogging.class}, handler);
@@ -21,41 +21,37 @@ public class Ioc {
 
 
     static class DemoInvocationHandler implements InvocationHandler {
-        private final TestLogging testLogging;
-        private final List<Method> methods = new ArrayList<>();
+        private final Object instance;
+        private final Set<String> methods = new HashSet<>();
 
-        DemoInvocationHandler(TestLogging testLogging) {
-            this.testLogging = testLogging;
+        DemoInvocationHandler(Object instance) {
+            this.instance = instance;
 
-            for (Method method : TestLoggingImpl.class.getDeclaredMethods()) {
+            for (Method method : instance.getClass().getDeclaredMethods()) {
                 if (method.isAnnotationPresent(Log.class)) {
-                    methods.add(method);
+                    methods.add(method.getName() + Arrays.toString(method.getParameterTypes()));
                 }
             }
         }
 
         @Override
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-            for (Method item : methods) {
-                Class<?>[] parameterTypes = item.getParameterTypes();
-                Class<?>[] parameterTypes1 = method.getParameterTypes();
-                if (item.getName().equals(method.getName())
-                        && Arrays.equals(parameterTypes, parameterTypes1))
-                {
-                    System.out.print("executed method:" + item.getName());
-                    for (int n = 0; n < args.length; n++) {
-                        System.out.print(", param" + (n + 1) + "=" + args[n]);
-                    }
-                    System.out.println();
+            String key = method.getName() + Arrays.toString(method.getParameterTypes());
+            if (methods.contains(key)) {
+                System.out.print("executed method:" + key);
+
+                for (int n = 0; n < args.length; n++) {
+                    System.out.print(", param" + (n + 1) + "=" + args[n]);
                 }
+                System.out.println();
             }
-            return method.invoke(testLogging, args);
+            return method.invoke(instance, args);
         }
 
         @Override
         public String toString() {
             return "DemoInvocationHandler {" +
-                    "testLoggingClass = " + testLogging +
+                    "testLoggingClass = " + instance +
                     '}';
         }
     }
