6fe655381dddc9d1d7c7f22c3c94bd7d3b5e16c1
asamara56 <a_g93@mail.ru>
2022-04-14 12:05:31 +0300

    hw21-jpql

--- hw21-jpql/src/main/java/ru/otus/crm/model/Client.java
+++ hw21-jpql/src/main/java/ru/otus/crm/model/Client.java
@@ -1,12 +1,9 @@
 package ru.otus.crm.model;
 
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "client")
@@ -14,11 +11,17 @@ public class Client implements Cloneable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    @Column(name = "id")
     private Long id;
 
     @Column(name = "name")
     private String name;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true, mappedBy = "client")
+    private List<Phone> phones;
 
     public Client() {
     }
@@ -33,17 +36,20 @@ public class Client implements Cloneable {
         this.name = name;
     }
 
+    public Client(Long id, String name, Address address, List<Phone> phones) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.setPhones(phones);
+    }
+
     @Override
     public Client clone() {
-        return new Client(this.id, this.name);
+        return new Client(this.id, this.name, this.address, this.phones);
     }
 
     public Long getId() {
         return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getName() {
@@ -54,11 +60,25 @@ public class Client implements Cloneable {
         this.name = name;
     }
 
+    public List<Phone> getPhones() {
+        return phones != null ? phones : new ArrayList<>();
+    }
+
+    public void setPhones(List<Phone> phones) {
+        this.phones = phones;
+
+        if (this.phones != null) {
+            this.phones.forEach(ph -> ph.setClient(this));
+        }
+    }
+
     @Override
     public String toString() {
         return "Client{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
+                ", address=" + address +
+                ", phones=" + phones +
                 '}';
     }
 }
